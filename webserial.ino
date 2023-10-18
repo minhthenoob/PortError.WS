2f4650643587a0b5d3ff16ebddcc5c27f163543e
minhthenoob <70839063+minhthenoob@users.noreply.github.com>
2023-10-18 17:17:21 +0700

    Add files via upload

---
+++ webserial.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+
+#define LED 2
+
+AsyncWebServer server(80);
+
+const char* ssid = "Mr Phone";          // Your WiFi SSID
+const char* password = "peanutbutters";  // Your WiFi Password
+
+void recvMsg(uint8_t *data, size_t len){
+  WebSerial.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+  if (d == "ON"){
+    digitalWrite(LED, LOW);
+  }
+  if (d=="OFF"){
+    digitalWrite(LED, HIGH);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg);
+  server.begin();
+}
+
+void loop() {
+  WebSerial.println("Hello!");
+  delay(2000);
+}
