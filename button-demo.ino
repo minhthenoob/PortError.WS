96a1548e3516981259314f43772cbe51e873ed0c
minhthenoob <70839063+minhthenoob@users.noreply.github.com>
2023-10-24 01:16:00 +0700

    Add files via upload

---
+++ button-demo.ino
@@ -0,0 +1,25 @@
+const int buttonPin = 2;  // the number of the pushbutton pin
+
+// variables will change:
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
